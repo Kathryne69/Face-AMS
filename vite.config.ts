a3f07cfd426b12003eccbf86fe6e4f2d8c07a930
Kathryne69 <raizendevs2023@gmail.com>
2025-03-20 23:48:20 +0800

    vitefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Face-AMS/", // 👈 Add your repository name here
   plugins: [react()],
-})
+});
