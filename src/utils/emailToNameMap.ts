6732c51eb76520bd6b8f78f3a12d351d74035e5c
SHION <raizendevs2023@gmail.com>
2025-04-23 22:58:18 +0800

    Update emailToNameMap.ts

--- src/utils/emailToNameMap.ts
+++ src/utils/emailToNameMap.ts
@@ -1,20 +1,6 @@
 // utils/emailToNameMap.ts
 const emailToNameMap: { [key: string]: string } = {
-    "student1@face.ams": "Dela Pieza, Mark Jaspher",
-    "student2@face.ams": "Navarro, Jules Rhenz",
-    "student3@face.ams": "Timbang, Romeo Felipe",
-    "student4@face.ams": "Orlain, Dwayne Regine",
-    "student5@face.ams": "Buscaino, Jed",
-    "student6@face.ams": "Ampong, Ronel",
-    "student7@face.ams": "Bolante, Joed",
-    "student8@face.ams": "Girado, Jerico",
-    "student9@face.ams": "Manalo, Steinberg",
-    "student10@face.ams": "Martinez, Shawn Erwin",
-    "student11@face.ams": "Gabriel, Prince Railey",
-    "student12@face.ams": "Piqueras, Cristan",
-    "student13@face.ams": "Decial, Jerry",
-    "student14@face.ams": "Vidallo, Melquisedec",
-    "student15@face.ams": "Tatlonghari, Daniel",
+    
 };
 
 export default emailToNameMap;
