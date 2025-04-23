360f7ccfa0f984ffe439c012339d8230dc3274d7
SHION <raizendevs2023@gmail.com>
2025-04-23 22:57:19 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,16 +10,6 @@ import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { initializeApp } from "firebase/app";
 import AuthRoute from './AuthRoute.tsx';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCbRdbjZWY36SODtKK9kjw0sZ2Ro84rCxs",
-  authDomain: "face-ams-1fa9f.firebaseapp.com",
-  databaseURL: "https://face-ams-1fa9f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "face-ams-1fa9f",
-  storageBucket: "face-ams-1fa9f.firebasestorage.app",
-  messagingSenderId: "366142425812",
-  appId: "1:366142425812:web:a324fa462502a8d1b2c941"
-};
-
 initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
