f830e35f4f8fcdcbae0ce4f04c99de752151b12c
Kathryne69 <raizendevs2023@gmail.com>
2025-05-06 13:04:45 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,6 @@ import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { initializeApp } from "firebase/app";
 import AuthRoute from './AuthRoute.tsx';
 
-initializeApp(firebaseConfig);
-
 const firebaseConfig = {
   apiKey: "AIzaSyCbRdbjZWY36SODtKK9kjw0sZ2Ro84rCxs",
   authDomain: "face-ams-1fa9f.firebaseapp.com",
@@ -21,6 +19,8 @@ const firebaseConfig = {
   messagingSenderId: "366142425812",
   appId: "1:366142425812:web:a324fa462502a8d1b2c941"
 };
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
