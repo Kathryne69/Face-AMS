c7456cbdee36272c0c16b6dd2159fd4253033a47
Kathryne69 <raizendevs2023@gmail.com>
2025-05-06 13:22:17 +0800

    emails

--- src/utils/emailToNameMap.ts
+++ src/utils/emailToNameMap.ts
@@ -1,20 +1,58 @@
-// utils/emailToNameMap.ts
-const emailToNameMap: { [key: string]: string } = {
-    "student1@face.ams": "Dela Pieza, Mark Jaspher",
-    "student2@face.ams": "Navarro, Jules Rhenz",
-    "student3@face.ams": "Timbang, Romeo Felipe",
-    "student4@face.ams": "Orlain, Dwayne Regine",
-    "student5@face.ams": "Buscaino, Jed",
-    "student6@face.ams": "Ampong, Ronel",
-    "student7@face.ams": "Bolante, Joed",
-    "student8@face.ams": "Girado, Jerico",
-    "student9@face.ams": "Manalo, Steinberg",
-    "student10@face.ams": "Martinez, Shawn Erwin",
-    "student11@face.ams": "Gabriel, Prince Railey",
-    "student12@face.ams": "Piqueras, Cristan",
-    "student13@face.ams": "Decial, Jerry",
-    "student14@face.ams": "Vidallo, Melquisedec",
-    "student15@face.ams": "Tatlonghari, Daniel",
-};
-
-export default emailToNameMap;
+const names = [
+    "Ampong, Ronel",
+    "Anedez, Ryan Mark",
+    "Baraero, Sean Michael",
+    "Biblanias, Yzza",
+    "Bolante, Joed",
+    "Bracero, Charlz Ivan",
+    "Buscaino, Jed",
+    "Caracas, Daniel Lloyd",
+    "Celimen, John Renz",
+    "Chavez, Dan Khervin",
+    "Conlu, Joneil",
+    "De Luna, Jed Erione",
+    "De Vera, Aubrey Kyla",
+    "Decial, Jerry",
+    "Dela Pieza, Mark Jaspher",
+    "Eluna, John Dietrich",
+    "Gabriel, Prince Railey",
+    "Lapuz, Johann Erl",
+    "Llegue, Richard Omie",
+    "Lopez, Jasffer John",
+    "Magsino, Arravellah",
+    "Magyaya, John Albert",
+    "Malana, Mary Joy",
+    "Manalo, Steinberg",
+    "Martinez, Shawn Erwin",
+    "Millena, John Joseph",
+    "Navarro, Jules Rhenz",
+    "Non, Elliser",
+    "Orbida, Nethouie Ljoy Angelo",
+    "Pable, Joevani",
+    "Paloca, Vince Eli",
+    "Parala, Arbie",
+    "Piqueras, Cristan",
+    "Pulido, John Lexter",
+    "Recena, Aira Mae",
+    "Regachuelo, John Cris",
+    "Renomeron, Fritz Mathew",
+    "Reyes, Roel Vincent",
+    "Roldan, Patrick Owen",
+    "Sacurat, Norjane Kyla",
+    "Sardoma, Joseph",
+    "Sayat, Reian",
+    "Tatlonghari, Daniel",
+    "Timbang, Romeo Felipe",
+    "Torda, Kaide Kerl",
+    "Valisno, Christ Mathew",
+    "Vidallo, Melquisedec"
+  ];
+  
+  const emailToNameMap: { [key: string]: string } = {};
+  
+  names.forEach((name, index) => {
+    emailToNameMap[`student${index + 1}@face.ams`] = name;
+  });
+  
+  export default emailToNameMap;
+  
