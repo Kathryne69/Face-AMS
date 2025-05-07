06cfc29ec77cb748bef65a9c88120f9fdcfcd44f
Shion <raizendevs2023@gmail.com>
2025-05-07 21:48:23 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,13 @@ import { initializeApp } from "firebase/app";
 import AuthRoute from './AuthRoute.tsx';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbRdbjZWY36SODtKK9kjw0sZ2Ro84rCxs",
-  authDomain: "face-ams-1fa9f.firebaseapp.com",
-  databaseURL: "https://face-ams-1fa9f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "face-ams-1fa9f",
-  storageBucket: "face-ams-1fa9f.firebasestorage.app",
-  messagingSenderId: "366142425812",
-  appId: "1:366142425812:web:a324fa462502a8d1b2c941"
+  apiKey: "SECRET",
+  authDomain: "SECRET",
+  databaseURL: "SECRET",
+  projectId: "SECRET",
+  storageBucket: "SECRET",
+  messagingSenderId: "SECRET",
+  appId: "SECRET"
 };
 
 initializeApp(firebaseConfig);
